626e23c503eb4d1c9578a9f826ac7d5f534dd671
Patryk Kożuch <patryk.kozuch.1@gmail.com>
2025-10-13 20:39:30 +0200

    Add environmental variables management

---
+++ Solutions/lab01/lab/main.py
@@ -0,0 +1,32 @@
+import os
+import argparse
+from dotenv import load_dotenv
+from settings import Settings
+
+
+def export_envs(environment: str = "dev") -> None:
+    env_file = f".env.{environment}"
+    if os.path.exists(env_file):
+        load_dotenv(env_file)
+    else:
+        raise FileNotFoundError(f"{env_file} does not exist.")
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="Load environment variables from specified.env file."
+    )
+    parser.add_argument(
+        "--environment",
+        type=str,
+        default="dev",
+        help="The environment to load (dev, test, prod)",
+    )
+    args = parser.parse_args()
+
+    export_envs(args.environment)
+
+    settings = Settings()
+
+    print("APP_NAME: ", settings.APP_NAME)
+    print("ENVIRONMENT: ", settings.ENVIRONMENT)
